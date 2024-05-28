cc5f91f3647c6292f8b76faafe6e1c6d12915617
prem-24 <prem24052000@gmail.com>
2024-05-28 23:06:20 +0530

    c

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-// ?
+// let a =  [2,3,2,2,3,2,3];
+
+// a.length = 0;
+// console.log(a);
+
+// const num = 8;
+// const obj = {
+//   num: 10,
+//   inner: {
+//   num: 6,
+//     getNum: function() {
+//       return this.num;
+//     }
+//   }
+// };
+// console.log(obj.inner.getNum());
+// const getNum = obj.inner.getNum;
+// console.log(getNum);
+
+
+
+
+// (function (x) {
+//     return (function (y) {
+//     console.log(x) ;
+//     }) (2) ;
+//     }) (1) ;
